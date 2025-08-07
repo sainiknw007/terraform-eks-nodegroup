a574339b0cd1c7e0324e856abd39a09bef0f7df8
Akhilesh Saini <146843314+sainiknw007@users.noreply.github.com>
2025-08-07 13:02:37 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
-  default     = "ankit-cluster" # optional: remove if you want to enforce providing it via tfvars
+  default     = "akhilesh-cluster" # optional: remove if you want to enforce providing it via tfvars
 }
